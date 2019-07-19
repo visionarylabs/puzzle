115448695cc9f1bd94f4c8e2374a7daa87638097
Ben <ben@visionarylabs.com>
2019-07-19 15:01:40 -0700

    create index file to kick off project

---
+++ docs/index.php
@@ -0,0 +1,15 @@
+<?php
+  /* main index for puzzle game */
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <h1>Puzzle</h1>
+    <p>Visionary Labs Public Puzzle Project</p>
+    <div class="stage">
+      <canvas id="game" class="main-canvas"></canvas>
+    </div>
+  </body>
+</html>
