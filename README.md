c9c9b744a7d606ac01f3f0948b75dfd51f58d006
opaladmin <opaladmin@cushman.dreamhost.com>
2019-08-26 10:22:00 -0700

    R6 - idea for tile builder

--- README.md
+++ README.md
@@ -7,62 +7,52 @@ https://visionarylabs.github.io/puzzle/
 
 ## MakeShpaes
 
-    Ignore rotation and symmetry for now, just focus and getting every possible shape
-    
-    1.
-    start with the straight vertical shape, build tiles down column #1 to make that shape
-    x
-    x
-    x
-    x
-    x
-    
-    1 new piece
-    
-    2.
-    then, shorten the left edge to n-1 (e.g. 4 for pentominoes)
-    then build down that edge, and the last tile will go to column #2, Row #1
-    then move that down column #2 until it reaches the end of the new edge (e.g. 4 for pentominoes)
-    xx  x   x   x
-    x   xx  x   x
-    x   x   xx  x
-    x   x   x   xx
-    
-    2 new pieces
-    
-    3.
-    shorten the edge again, now we have 2 remaining tiles for pentominoes.
-    place the first in spot 2,1 - then start moving the second one down to the end of the edge.
-    then place the first in spot 2,2 - then move the second down the edge
-    keep moving the first down all the way to the end, and continue to add the 2nd one even past the edge.
-    xx  xx  x   x
-    xx  x   xx  x
-    x   xx  xx  xx
-                 x
-                 
-    3 new pieces
-    
-    3.1
-    start moving to the 3rd column
-    xxx x   x
-    x   xxx x
-    x   x   xxx
-    
-    2 new pieces
-    
-    4.
-    left edge of 2 with max height of 3, and no 3 edgers
-    x   x   x
-    xxx xx  xxx
-     x   xx   x
-    
-    3 new pieces
-     
-    5.
-    the no corner piece
-    build a no corner L then try every combo with no 2 edgers
-     x
-    xxx
-     x
-    
-    1 new piece
+Ignore rotation and symmetry for now, just focus and getting every possible shape
+
+n = number of tiles in this puzzle's shapes, e.g. n=5 for pentominoes
+s = the step you are on, 1xn, 2 x n-1, 3 x n-2 etc.
+z = the tile you are testing, 1 being the last, 2 being 2nd to last etc.
+g = the grid space you are testing for tile z, e.g. spot 3,4,5 etc
+
+start with a grid 1 x n, e.g. 1x5
+1 2 3 4 5
+
+place n tiles in each grid space until none are left, that was step 1
+xxxxx
+
+move to step 2, make a grid that is n - (s-1) x s, e.g. 4 x 2 for step 2
+fill in the first row, e.g. 1,2,3,4 with the first 4, then move that last tile to each space in row 2.
+1 2 3 4
+5 6 7 8
+
+xxxx
+x
+
+Check to make sure it's adjacent to a tile.
+after each space is checked, move the 2nd to last tile to a free space, then place the last tile in each adjacent space
+
+xxx
+xx
+
+xxx
+ xx
+
+xxx
+  xx
+  
+when the last spot is checked z--
+xx
+xxx
+
+x
+xxxx
+
+no need to check lower positions again after z is moved down
+then move to the next step
+e.g. 3x3, this is the last step because s cannot be less than half of n.
+1 2 3
+4 5 6
+7 8 9
+
+xxx
+xx
--- docs/js/puzzle.js
+++ docs/js/puzzle.js
@@ -11,109 +11,140 @@ var VPuzzle = function () {
     canvas.width = 700;
     canvas.height = 700;
     canvas.id = 'game';
-
-    //the main array of all shapes
-    var shapes = [];
-
-    var settings = {
-        tileCount : 4, //e.g. 4 for tetrominoes
-        tileSize : 25,
-        startingX : 20,
-        startingY : 50,
-    }
-
-    //set constants based on settins
-    //the rest of the shapes after shape 1 will fit in a n-2 x n-1 grid
-    var constants = {
-        startingX : 20,
-        maxXCount : settings.tileCount - 1,
-        maxYCount : settings.tileCount,
-    }
-
-    var buildState = {
-        //a state to save the tile position as we build each shape
-        tilePosY : 0, //the placement of the next tile
-        tilePosX : 0,
-        curRow : 0, //the current column to build down
-        curCol : 0, //the current row to build across
-        leftEdgeCount : constants.maxYCount, //the max Left Edge for the current loop
-        topEdgeCount : constants.maxXCount, //the max Top Edge for the current loop
-    }
-
+    
     // init
     var init = function () {
+        grid = makeGrid();
         makeShapes();
-        console.log('done making shapes');
+        console.log('*** done making shapes:');
         console.log(shapes);
         render();
     }
 
-/**
-    the main shapebuilder, recursive to build the next shape on the list
-**/
+    //the main array of all shapes
+    var shapes = [];
+    
+    var grid = [];
+
+    //set constants based on settins
+    var constants = {
+        startingX : 20,
+        startingY : 50
+    }
+    
+    var settings = {
+        tileCount : 4, //e.g. 4 for tetrominoes
+        tileSize : 25,
+        startingX : constants.startingX,
+        startingY : constants.startingY
+    }
+
+    //a state to save the tile positions as we build each shape
+    var state = {
+        
+        step : 1, //step 1 is the full horizantal line
+        
+        //start with a n x 1 grid
+        gridMaxX : settings.tileCount,
+        gridMaxY : 1,
+        
+        curTile : 1, //the current tile in the loop
+        curSpace : 1, //the current space in the loop
+        testTile : 1, //the tile being tested e.g. the last tile
+        testSpace : 1, //the space being test, the next available space
+        
+    }
+    
+    var grid = null;
+
+    /**
+        the main shapebuilder, recursive to build the next shape on the list
+         *************************************************************************************
+    **/
     var makeShapes = function () {
         var shape = [];
         var tile = [];
         var i = 0;
-        var tileId = 0;
+        
+        var fixedRow = settings.tileCount - (state.step - 1);
+        var tilesLeft = state.step - 1;
 
+        if( state.gridMaxY > Math.ceil(settings.tileCount / 2) ) return;
         if( 12 === shapes.length ) return; //fail safe
         
         console.log( 'BUILDING SHAPE #' + Number(shapes.length + 1) );
+        console.log(grid);
 
         //the main loop to make all the rest of shapes
+        
         //loop for each tile in the shape tile count
+        for(i=0; i < fixedRow; i++){
 
-        for(i=0; i < settings.tileCount; i++){
+            state.curTile = i + 1;
+            state.curSpace = i + 1;
+            grid[i].checked = true;
+            grid[i].occupied = true;
 
-            tileId = i + 1;
-
-            //for now, all shapes start at 0,0
-            if(tileId === 1 ){
-                //reset x and y for this shape
-                buildState.tilePosY = 0;
-                buildState.tilePosX = 0;
-            }else{
-                //build down the column
-                buildState.tilePosY++;
-            }
-
-            //MAIN
-            if( tileId > buildState.leftEdgeCount ){
-                buildState.tilePosY = buildState.curRow;
-                buildState.tilePosX = buildState.curCol;
-            }
-            
-            //move the row count down the next col
-            if( tileId > buildState.leftEdgeCount ){
-                buildState.curRow++;
-            }
-
-            //do not allow tiles to go off the preset area
-            if(
-                buildState.tilePosX >= constants.maxXCount ||
-                buildState.tilePosY >= constants.maxYCount
-            ){
-                console.log( 'we have gone to far!!' );
-                return;
-            }
-
-            console.log( '...building tile ' + tileId + ':    ' + buildState.tilePosX + ', ' + buildState.tilePosY );
-            tile = [ buildState.tilePosX, buildState.tilePosY ];
+            //place the tile in this shape
+            console.log( '...placing tile ' + state.curTile);
+            tile = [ state.curSpace, 1 ];
             shape.push(tile);
 
+        }
+        
+        var nextCheckSpace = null;
+        for(i=0; i < grid.length; i++){
+            if(grid[i].checked == false){
+                nextCheckSpace = i;
+                break;
+            }
+        }
+        if(nextCheckSpace && tilesLeft > 0){
+            console.log('MOVING ON!');
+            console.log(nextCheckSpace);
+            tile = [ grid[nextCheckSpace].x , grid[nextCheckSpace].y ];
+            grid[nextCheckSpace].checked = true;
+            grid[nextCheckSpace].occupied = true;
+            shape.push(tile);
         }
 
         shapes.push(shape);//add the last shape to the list
 
-        //look at the last shape position and move on to the next max edge
-        if(buildState.tilePosY === buildState.leftEdgeCount - 1){
-            buildState.leftEdgeCount--;
-            buildState.curCol++;
+        var availableSpace = false;
+        for(i=0; i < grid.length; i++){
+            if(grid[i].occupied == false){
+                availableSpace = true;
+                break;
+            }
+        }
+        
+        //check to move state to next shape
+        if( !availableSpace ){
+            state.step++;
+            state.gridMaxY = state.step;
+            grid = makeGrid();
         }
 
-        makeShapes(); //call again or the next shape
+        makeShapes(); //call again or the next shape 
 
+    }
+    /**
+        END THE MAIN PUZZLE MAKER
+        *************************************************************************************
+    **/
+    
+    //make a grid, this is the grid to check against for the current step
+    var makeGrid = function () {
+        var g = [];
+        for(y = 1; y <= state.gridMaxY; y++){
+            for(x = 1; x <= state.gridMaxX; x++){
+                space = {x:x,y:y,occupied:false,checked:false};
+                g.push(space);
+            }
+        }
+        console.log('made grid:')
+        console.log(g);
+        return g;
     }
 
     //Drawings and Rendering Functions
@@ -132,21 +163,26 @@ var VPuzzle = function () {
 
     //draw all the shpaes in the list
     var drawShapes = function () {
+        console.log('drawing all shapes...');
         var i = 0;
         var count = shapes.length;
         for(i=0; i < count; i++){
-            //console.log('drawing shape #' + i);
-            //console.log(shapes[i]);
             drawShape( shapes[i] );
-
-            //position the next shape draw in next spot on canvas
-            settings.startingX += (settings.tileSize * (constants.maxXCount + 1) );
-            if( settings.startingX + (settings.tileSize * (constants.maxXCount + 1) ) > canvas.width ){
-                settings.startingX = constants.startingX;
-                settings.startingY += (settings.tileSize * (constants.maxYCount + 1) );
-            }
-
+            resetPosition();
         }
+    }
+    
+    //position the next shape draw in next spot on canvas
+    var resetPosition = function () {
+
+        settings.startingX += (settings.tileSize * (settings.tileCount + 1) );
+
+        //if it goes off the edge move down in Y
+        if( settings.startingX + (settings.tileSize * (settings.tileCount) ) > canvas.width ){
+            settings.startingX = constants.startingX;
+            settings.startingY += (settings.tileSize * (settings.tileCount + 1) );
+        }
+
     }
 
     //draw a shape based on an x,y tile position array
@@ -157,8 +193,8 @@ var VPuzzle = function () {
         var i = 0;
         var count = shape.length;
         for(i=0; i < count; i++){
-            x = settings.startingX + (shape[i][0]) * settings.tileSize;
-            y = settings.startingY + (shape[i][1]) * settings.tileSize;
+            x = settings.startingX + (shape[i][0]) * settings.tileSize - settings.tileSize;
+            y = settings.startingY + (shape[i][1]) * settings.tileSize - settings.tileSize;
             drawTile( x, y );
         }
     }
@@ -176,7 +212,7 @@ var VPuzzle = function () {
     //draw the background, the max area for this shape builder
     var drawBackground = function ( x, y ) {
         ctx.fillStyle = "rgb(65,65,65)";
-        ctx.fillRect( x, y, (settings.tileSize * constants.maxXCount), (settings.tileSize * constants.maxYCount) );
+        ctx.fillRect( x, y, (settings.tileSize * settings.tileCount), (settings.tileSize * settings.tileCount) );
     }
 
     return {
